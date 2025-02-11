fde6aee25ef86fbb42572a687c561dc241dc7b98
Min Thant <minnthantwinn@gmail.com>
2025-02-11 23:32:59 +0630

    error - transfer to min thiha naing

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Login
+Route::post("login", [AuthController::class, "login"]);
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
