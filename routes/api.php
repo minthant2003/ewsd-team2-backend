3154f48a963a1819a0eeb4df85fdef36f38494cc
Zue Mi Mi Aung <zmma@mit.com.mm>
2025-04-05 22:07:41 +0630

    add Last logged in user and Browser Type List

--- routes/api.php
+++ routes/api.php
@@ -119,4 +119,11 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     // Api for most viewed ideas QA manager
     Route::get('/getMostViewedIdeas/{academicYearId}', [SystemReportController::class, 'getMostViewedIdeas']);
+
+    //Last Login User
+    Route::get("/getLastLoginUser", [AuthController::class, "getLastLoggedInUser"]);
+
+    // Log Browser
+    Route::post('/log-browser', [AuthController::class, 'logUserBrowser']);
+    Route::get('/logged-in-browsers', [AuthController::class, 'browserList']);
 });
